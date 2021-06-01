e15d5eb45182685b85956e8a13b5e39741bc93eb
emeMig <miguel.monedo@gmail.com>
2021-06-01 12:08:21 +0200

    ccc

--- src/views/Tables/projects.js
+++ src/views/Tables/projects.js
@@ -71,5 +71,221 @@ export default [
     status: 'completed',
     statusType: 'success',
     completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 1',
+    budget: 'LOCALIZACION',
+    status: 'pending',
+    statusType: 'warning',
+    completion: 60
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 2',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 3',
+    budget: 'LOCALIZACION',
+    status: 'delayed',
+    statusType: 'danger',
+    completion: 72
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 4',
+    budget: 'LOCALIZACION',
+    status: 'on schedule',
+    statusType: 'info',
+    completion: 90
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 5',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 6',
+    budget: 'LOCALIZACION',
+    status: 'pending',
+    statusType: 'warning',
+    completion: 60
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 7',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 8',
+    budget: 'LOCALIZACION',
+    status: 'delayed',
+    statusType: 'danger',
+    completion: 72
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO9',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 1',
+    budget: 'LOCALIZACION',
+    status: 'pending',
+    statusType: 'warning',
+    completion: 60
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 2',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 3',
+    budget: 'LOCALIZACION',
+    status: 'delayed',
+    statusType: 'danger',
+    completion: 72
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 4',
+    budget: 'LOCALIZACION',
+    status: 'on schedule',
+    statusType: 'info',
+    completion: 90
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 5',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 6',
+    budget: 'LOCALIZACION',
+    status: 'pending',
+    statusType: 'warning',
+    completion: 60
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 7',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 8',
+    budget: 'LOCALIZACION',
+    status: 'delayed',
+    statusType: 'danger',
+    completion: 72
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO9',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 1',
+    budget: 'LOCALIZACION',
+    status: 'pending',
+    statusType: 'warning',
+    completion: 60
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 2',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 3',
+    budget: 'LOCALIZACION',
+    status: 'delayed',
+    statusType: 'danger',
+    completion: 72
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 4',
+    budget: 'LOCALIZACION',
+    status: 'on schedule',
+    statusType: 'info',
+    completion: 90
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 5',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 6',
+    budget: 'LOCALIZACION',
+    status: 'pending',
+    statusType: 'warning',
+    completion: 60
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 7',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO 8',
+    budget: 'LOCALIZACION',
+    status: 'delayed',
+    statusType: 'danger',
+    completion: 72
+  },
+  {
+    img: 'img/theme/team-1.png',
+    title: 'ELEMENTO9',
+    budget: 'LOCALIZACION',
+    status: 'completed',
+    statusType: 'success',
+    completion: 100
   }
 ]
